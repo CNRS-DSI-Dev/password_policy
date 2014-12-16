fc37f8cb846ec11eb27335a45b4391126da59c00
Patrick Paysant <patrick.paysant@linagora.com>
2014-12-16 16:09:26 +0100

    Quick fix db storage to use oc_appconfig instead of app specific table (multimaster compatible).

--- lib/password_policy.php
+++ lib/password_policy.php
@@ -45,183 +45,213 @@ class OC_Password_Policy {
 
 	public static function setMinLength($limit){
 
-		//check if record already exists
-		$sql = "select * from `*PREFIX*password_policy_items` where id=1";
-		$query = OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// //check if record already exists
+		// $sql = "select * from `*PREFIX*password_policy_items` where id=1";
+		// $query = OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		if ($row = $result->fetchRow()){
-			//update
-			$sql = "update `*PREFIX*password_policy_items` set min_length=? where id=1";
-		}
-		else
-		{//insert new
-			$sql = "insert into `*PREFIX*password_policy_items` (min_length) values (?)";
-		}
+		// if ($row = $result->fetchRow()){
+		// 	//update
+		// 	$sql = "update `*PREFIX*password_policy_items` set min_length=? where id=1";
+		// }
+		// else
+		// {//insert new
+		// 	$sql = "insert into `*PREFIX*password_policy_items` (min_length) values (?)";
+		// }
 
-		$query = OCP\DB::prepare($sql);
-		$query->bindParam(1, $limit, \PDO::PARAM_INT);
-		$result = $query->execute();
+		// $query = OCP\DB::prepare($sql);
+		// $query->bindParam(1, $limit, \PDO::PARAM_INT);
+		// $result = $query->execute();
+
+		$appConfig = \OC::$server->getAppConfig();
+        $result = $appConfig->setValue('password_policy', 'min_length', $limit);
 	}
 
 	public static function setSpecialCharsList($list){
-		//check if record already exists
-		$sql = "select * from `*PREFIX*password_policy_items` where id=1";
-		$query = OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// //check if record already exists
+		// $sql = "select * from `*PREFIX*password_policy_items` where id=1";
+		// $query = OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		if ($row = $result->fetchRow()){
-			//update
-			$sql = 'update `*PREFIX*password_policy_items` set specialcharslist=? where id=1';
-		}
-		else
-		{//insert new
+		// if ($row = $result->fetchRow()){
+		// 	//update
+		// 	$sql = 'update `*PREFIX*password_policy_items` set specialcharslist=? where id=1';
+		// }
+		// else
+		// {//insert new
 
-			$sql = 'insert into `*PREFIX*password_policy_items` (specialcharslist) values (?)';
-		}
+		// 	$sql = 'insert into `*PREFIX*password_policy_items` (specialcharslist) values (?)';
+		// }
 
-		$query = OCP\DB::prepare($sql);
-		$query->bindParam(1, $list, \PDO::PARAM_STR);
-		$result = $query->execute();
+		// $query = OCP\DB::prepare($sql);
+		// $query->bindParam(1, $list, \PDO::PARAM_STR);
+		// $result = $query->execute();
+
+		$appConfig = \OC::$server->getAppConfig();
+        $result = $appConfig->setValue('password_policy', 'specialcharslist', $list);
 	}
 
 	public static function setSpecialChars($specialcharsrequired)
 	{
-		$sql = "select * from `*PREFIX*password_policy_items` where id=1";
-		$query = OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $sql = "select * from `*PREFIX*password_policy_items` where id=1";
+		// $query = OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		if ($row = $result->fetchRow()){
-			//update
-			$sql = "update `*PREFIX*password_policy_items` set specialcharacters=? where id=1";
-		}
-		else
-		{//insert new
-			$sql = "insert into `*PREFIX*password_policy_items` (specialcharacters) values (?)";
-		}
+		// if ($row = $result->fetchRow()){
+		// 	//update
+		// 	$sql = "update `*PREFIX*password_policy_items` set specialcharacters=? where id=1";
+		// }
+		// else
+		// {//insert new
+		// 	$sql = "insert into `*PREFIX*password_policy_items` (specialcharacters) values (?)";
+		// }
 
-		$query = OCP\DB::prepare($sql);
-		$query->bindParam(1, $specialcharsrequired, \PDO::PARAM_INT);
-		$result = $query->execute();
+		// $query = OCP\DB::prepare($sql);
+		// $query->bindParam(1, $specialcharsrequired, \PDO::PARAM_INT);
+		// $result = $query->execute();
+
+		$appConfig = \OC::$server->getAppConfig();
+        $result = $appConfig->setValue('password_policy', 'specialcharacters', $specialcharsrequired);
 	}
 
 	public static function setMixedCase($mixedcase)
 	{
-		$sql = "select * from `*PREFIX*password_policy_items` where id=1";
-		$query = OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $sql = "select * from `*PREFIX*password_policy_items` where id=1";
+		// $query = OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		if ($row = $result->fetchRow()){
-			//update
-			$sql = "update `*PREFIX*password_policy_items` set mixedcase=? where id=1";
-		}
-		else
-		{//insert new
-			$sql = "insert into `*PREFIX*password_policy_items` (mixedcase) values (?)";
-		}
+		// if ($row = $result->fetchRow()){
+		// 	//update
+		// 	$sql = "update `*PREFIX*password_policy_items` set mixedcase=? where id=1";
+		// }
+		// else
+		// {//insert new
+		// 	$sql = "insert into `*PREFIX*password_policy_items` (mixedcase) values (?)";
+		// }
 
-		$query = OCP\DB::prepare($sql);
-		$query->bindParam(1, $mixedcase, \PDO::PARAM_INT);
-		$result = $query->execute();
+		// $query = OCP\DB::prepare($sql);
+		// $query->bindParam(1, $mixedcase, \PDO::PARAM_INT);
+		// $result = $query->execute();
+
+		$appConfig = \OC::$server->getAppConfig();
+        $result = $appConfig->setValue('password_policy', 'mixedcase', $mixedcase);
 	}
 
 	public static function setNumbers($numbers)
 	{
-		$sql = "select * from `*PREFIX*password_policy_items` where id=1";
-		$query = OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $sql = "select * from `*PREFIX*password_policy_items` where id=1";
+		// $query = OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		if ($row = $result->fetchRow()){
-			//update
-			$sql = "update `*PREFIX*password_policy_items` set numbers=? where id=1";
-		}
-		else
-		{//insert new
-			$sql = "insert into `*PREFIX*password_policy_items` (numbers) values (?)";
-		}
+		// if ($row = $result->fetchRow()){
+		// 	//update
+		// 	$sql = "update `*PREFIX*password_policy_items` set numbers=? where id=1";
+		// }
+		// else
+		// {//insert new
+		// 	$sql = "insert into `*PREFIX*password_policy_items` (numbers) values (?)";
+		// }
 
-		$query = OCP\DB::prepare($sql);
-		$query->bindParam(1, $numbers, \PDO::PARAM_INT);
-		$result = $query->execute();
+		// $query = OCP\DB::prepare($sql);
+		// $query->bindParam(1, $numbers, \PDO::PARAM_INT);
+		// $result = $query->execute();
+
+		$appConfig = \OC::$server->getAppConfig();
+        $result = $appConfig->setValue('password_policy', 'numbers', $numbers);
 	}
 
 	public static function getMinLength(){
-		$sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
+		// $sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
 
-		$query = \OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $query = \OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		//default to 15
-		$min_length = 15;
+		// //default to 15
+		// $min_length = 15;
 
-		while($row = $result->fetchRow()) {
-			$min_length = $row['min_length'];
-		}
+		// while($row = $result->fetchRow()) {
+		// 	$min_length = $row['min_length'];
+		// }
+
+		$appConfig = \OC::$server->getAppConfig();
+        $min_length = $appConfig->getValue('password_policy', 'min_length', 15);
 
 		return $min_length;
 	}
 
 	public static function getSpecialCharsList(){
-		$sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
+		// $sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
 
-		$query = \OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $query = \OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		//default special chars list
-		$specialcharslist = "";
+		// //default special chars list
+		// $specialcharslist = "";
 
-		while($row = $result->fetchRow()) {
-			$specialcharslist = $row['specialcharslist'];
-		}
+		// while($row = $result->fetchRow()) {
+		// 	$specialcharslist = $row['specialcharslist'];
+		// }
+
+		$appConfig = \OC::$server->getAppConfig();
+        $specialcharslist = $appConfig->getValue('password_policy', 'specialcharslist', '');
 
 		return $specialcharslist;
 	}
 
 	public static function getSpecialChars(){
-		$sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
+		// $sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
 
-		$query = \OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $query = \OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		//default special chars list
-		$specialcharslist = true;
+		// //default special chars list
+		// $specialcharslist = true;
 
-		$specialcharacters = '';
-		while($row = $result->fetchRow()) {
-			$specialcharacters = $row['specialcharacters'];
-		}
+		// $specialcharacters = '';
+		// while($row = $result->fetchRow()) {
+		// 	$specialcharacters = $row['specialcharacters'];
+		// }
+
+		$appConfig = \OC::$server->getAppConfig();
+        $specialcharacters = $appConfig->getValue('password_policy', 'specialcharacters', 1);
 
 		return $specialcharacters;
 	}
 
 	public static function getMixedCase(){
-		$sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
+		// $sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
 
-		$query = \OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $query = \OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		//default special chars list
-		$mixedcase = true;
+		// //default special chars list
+		// $mixedcase = true;
 
-		while($row = $result->fetchRow()) {
-			$mixedcase = $row['mixedcase'];
-		}
+		// while($row = $result->fetchRow()) {
+		// 	$mixedcase = $row['mixedcase'];
+		// }
+
+		$appConfig = \OC::$server->getAppConfig();
+        $mixedcase = $appConfig->getValue('password_policy', 'mixedcase', true);
 
 		return $mixedcase;
 	}
 
 	public static function getNumbers(){
-		$sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
+		// $sql = 'SELECT * FROM `*PREFIX*password_policy_items` WHERE id =1';
 
-		$query = \OCP\DB::prepare($sql);
-		$result = $query->execute();
+		// $query = \OCP\DB::prepare($sql);
+		// $result = $query->execute();
 
-		//default special chars list
-		$numbers = true;
+		// //default special chars list
+		// $numbers = true;
 
-		while($row = $result->fetchRow()) {
-			$numbers = $row['numbers'];
-		}
+		// while($row = $result->fetchRow()) {
+		// 	$numbers = $row['numbers'];
+		// }
+
+		$appConfig = \OC::$server->getAppConfig();
+        $numbers = $appConfig->getValue('password_policy', 'numbers', true);
 
 		return $numbers;
 	}
